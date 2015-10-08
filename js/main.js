66f533afdf3cae7730a18b868d1bc569d1079380
amyoldfield <golf.gti.edition.30@gmail.com>
2015-10-08 21:57:01 +0100

    comment out unused js import and scripts

--- js/main.js
+++ js/main.js
@@ -134,100 +134,100 @@ jQuery(function($) {'use strict';
 	smoothScroll.init();
 
 	// portfolio filter
-	$(window).load(function(){'use strict';
-		var $portfolio_selectors = $('.portfolio-filter >li>a');
-		var $portfolio = $('.portfolio-items');
-		$portfolio.isotope({
-			itemSelector : '.portfolio-item',
-			layoutMode : 'fitRows'
-		});
-		
-		$portfolio_selectors.on('click', function(){
-			$portfolio_selectors.removeClass('active');
-			$(this).addClass('active');
-			var selector = $(this).attr('data-filter');
-			$portfolio.isotope({ filter: selector });
-			return false;
-		});
-	});
+	//$(window).load(function(){'use strict';
+	//	var $portfolio_selectors = $('.portfolio-filter >li>a');
+	//	var $portfolio = $('.portfolio-items');
+	//	$portfolio.isotope({
+	//		itemSelector : '.portfolio-item',
+	//		layoutMode : 'fitRows'
+	//	});
+	//
+	//	$portfolio_selectors.on('click', function(){
+	//		$portfolio_selectors.removeClass('active');
+	//		$(this).addClass('active');
+	//		var selector = $(this).attr('data-filter');
+	//		$portfolio.isotope({ filter: selector });
+	//		return false;
+	//	});
+	//});
 
-	$(document).ready(function() {
-		//Animated Progress
-		$('.progress-bar').bind('inview', function(event, visible, visiblePartX, visiblePartY) {
-			if (visible) {
-				$(this).css('width', $(this).data('width') + '%');
-				$(this).unbind('inview');
-			}
-		});
-
-		//Animated Number
-		$.fn.animateNumbers = function(stop, commas, duration, ease) {
-			return this.each(function() {
-				var $this = $(this);
-				var start = parseInt($this.text().replace(/,/g, ""));
-				commas = (commas === undefined) ? true : commas;
-				$({value: start}).animate({value: stop}, {
-					duration: duration == undefined ? 1000 : duration,
-					easing: ease == undefined ? "swing" : ease,
-					step: function() {
-						$this.text(Math.floor(this.value));
-						if (commas) { $this.text($this.text().replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "$1,")); }
-					},
-					complete: function() {
-						if (parseInt($this.text()) !== stop) {
-							$this.text(stop);
-							if (commas) { $this.text($this.text().replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "$1,")); }
-						}
-					}
-				});
-			});
-		};
-
-		$('.animated-number').bind('inview', function(event, visible, visiblePartX, visiblePartY) {
-			var $this = $(this);
-			if (visible) {
-				$this.animateNumbers($this.data('digit'), false, $this.data('duration')); 
-				$this.unbind('inview');
-			}
-		});
-	});
+	//$(document).ready(function() {
+	//	//Animated Progress
+	//	$('.progress-bar').bind('inview', function(event, visible, visiblePartX, visiblePartY) {
+	//		if (visible) {
+	//			$(this).css('width', $(this).data('width') + '%');
+	//			$(this).unbind('inview');
+	//		}
+	//	});
+    //
+	//	//Animated Number
+	//	$.fn.animateNumbers = function(stop, commas, duration, ease) {
+	//		return this.each(function() {
+	//			var $this = $(this);
+	//			var start = parseInt($this.text().replace(/,/g, ""));
+	//			commas = (commas === undefined) ? true : commas;
+	//			$({value: start}).animate({value: stop}, {
+	//				duration: duration == undefined ? 1000 : duration,
+	//				easing: ease == undefined ? "swing" : ease,
+	//				step: function() {
+	//					$this.text(Math.floor(this.value));
+	//					if (commas) { $this.text($this.text().replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "$1,")); }
+	//				},
+	//				complete: function() {
+	//					if (parseInt($this.text()) !== stop) {
+	//						$this.text(stop);
+	//						if (commas) { $this.text($this.text().replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "$1,")); }
+	//					}
+	//				}
+	//			});
+	//		});
+	//	};
+    //
+	//	$('.animated-number').bind('inview', function(event, visible, visiblePartX, visiblePartY) {
+	//		var $this = $(this);
+	//		if (visible) {
+	//			$this.animateNumbers($this.data('digit'), false, $this.data('duration'));
+	//			$this.unbind('inview');
+	//		}
+	//	});
+	//});
 
 	// Contact form
-	var form = $('#main-contact-form');
-	form.submit(function(event){
-		event.preventDefault();
-		var form_status = $('<div class="form_status"></div>');
-		$.ajax({
-			url: $(this).attr('action'),
-			beforeSend: function(){
-				form.prepend( form_status.html('<p><i class="fa fa-spinner fa-spin"></i> Email is sending...</p>').fadeIn() );
-			}
-		}).done(function(data){
-			form_status.html('<p class="text-success">Thank you for contact us. As early as possible  we will contact you</p>').delay(3000).fadeOut();
-		});
-	});
+	//var form = $('#main-contact-form');
+	//form.submit(function(event){
+	//	event.preventDefault();
+	//	var form_status = $('<div class="form_status"></div>');
+	//	$.ajax({
+	//		url: $(this).attr('action'),
+	//		beforeSend: function(){
+	//			form.prepend( form_status.html('<p><i class="fa fa-spinner fa-spin"></i> Email is sending...</p>').fadeIn() );
+	//		}
+	//	}).done(function(data){
+	//		form_status.html('<p class="text-success">Thank you for contact us. As early as possible  we will contact you</p>').delay(3000).fadeOut();
+	//	});
+	//});
 
 	//Pretty Photo
-	$("a[rel^='prettyPhoto']").prettyPhoto({
-		social_tools: false
-	});
+	//$("a[rel^='prettyPhoto']").prettyPhoto({
+	//	social_tools: false
+	//});
 
 	//Google Map
-	var latitude = $('#google-map').data('latitude');
-	var longitude = $('#google-map').data('longitude');
-	function initialize_map() {
-		var myLatlng = new google.maps.LatLng(latitude,longitude);
-		var mapOptions = {
-			zoom: 14,
-			scrollwheel: false,
-			center: myLatlng
-		};
-		var map = new google.maps.Map(document.getElementById('google-map'), mapOptions);
-		var marker = new google.maps.Marker({
-			position: myLatlng,
-			map: map
-		});
-	}
+	//var latitude = $('#google-map').data('latitude');
+	//var longitude = $('#google-map').data('longitude');
+	//function initialize_map() {
+	//	var myLatlng = new google.maps.LatLng(latitude,longitude);
+	//	var mapOptions = {
+	//		zoom: 14,
+	//		scrollwheel: false,
+	//		center: myLatlng
+	//	};
+	//	var map = new google.maps.Map(document.getElementById('google-map'), mapOptions);
+	//	var marker = new google.maps.Marker({
+	//		position: myLatlng,
+	//		map: map
+	//	});
+	//}
 	// google.maps.event.addDomListener(window, 'load', initialize_map);
 
 });
